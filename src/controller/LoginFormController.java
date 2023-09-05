cae4a4be49d03e5555549668f76a31828f93a628
Lahiru De Silva <lahirurashmitha00@gmail.com>
2023-09-05 23:18:24 +0530

    Complete login GUI

---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+    public TextField userNameTxt;
+    public PasswordField passwordTxt;
+
+    public void lognBtnOnActon(ActionEvent actionEvent) {
+    }
+}
