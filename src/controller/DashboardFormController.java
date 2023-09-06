cea446d25dfc69d56bfd477ccdfbb2f5b30cb102
Lahiru De Silva <lahirurashmitha00@gmail.com>
2023-09-07 00:25:18 +0530

    Update dashboard to set user name

--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -15,6 +16,11 @@ public class DashboardFormController {
     public AnchorPane contextDashboardForm;
     public Label userNameTxt;
     public Label incomeTxt;
+    private static String userName;
+
+    public void initialize() {
+        userNameTxt.setText(userName);
+    }
 
     private void setUI(String UI_Name) throws IOException {
         Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../view/" + UI_Name + ".fxml")));
@@ -23,7 +29,8 @@ public class DashboardFormController {
         stage.centerOnScreen();
     }
 
-    public void sellOnActionBtn(ActionEvent actionEvent) {
+    public void sellOnActionBtn(ActionEvent actionEvent) throws IOException {
+        setUI("SellForm");
     }
 
     public void itemOnActionBtn(ActionEvent actionEvent) {
@@ -33,5 +40,12 @@ public class DashboardFormController {
     }
 
     public void incomeOnActionBtn(ActionEvent actionEvent) {
+    }
+
+    public void userAccOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void setUserName(String userName) {
+        DashboardFormController.userName = userName;
     }
 }
