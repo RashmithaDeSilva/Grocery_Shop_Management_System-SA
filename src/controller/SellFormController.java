f7fcc4e7f90571dff9c20d06f364a052de18cdcd
Lahiru De Silva <lahirurashmitha00@gmail.com>
2023-09-07 01:18:47 +0530

    07/09/2023 - 01:18

--- src/controller/SellFormController.java
+++ src/controller/SellFormController.java
@@ -1,11 +1,18 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class SellFormController {
     public AnchorPane contextSellForm;
@@ -26,9 +33,17 @@ public class SellFormController {
     public void addOnAction(ActionEvent actionEvent) {
     }
 
-    public void backOnAction(ActionEvent actionEvent) {
+    public void backOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("DashboardForm");
     }
 
     public void sellOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUI(String UI_Name) throws IOException {
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../view/" + UI_Name + ".fxml")));
+        Stage stage = (Stage) contextSellForm.getScene().getWindow();
+        stage.setScene(new Scene(parent));
+        stage.centerOnScreen();
     }
 }
