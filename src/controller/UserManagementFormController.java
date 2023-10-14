4921a35498288365af78704ff8344cb58342acdd
Lahiru De Silva <lahirurashmitha00@gmail.com>
2023-10-14 21:21:04 +0530

    Complet user managemnt form GUI

--- src/controller/UserManagementFormController.java
+++ src/controller/UserManagementFormController.java
@@ -1,4 +1,23 @@
 package controller;
 
-public class UserManagementFormController {
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import model.Window;
+
+import java.sql.SQLException;
+
+
+public class UserManagementFormController extends Window {
+    public AnchorPane contextUserManagement;
+
+    public void initialize() {
+        super.context = contextUserManagement;
+    }
+
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addUsersOnAction(ActionEvent actionEvent) {
+    }
 }
